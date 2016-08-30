0aa44eb75adafd35b9d2b78fbaba6a63d50c706e
Ian McEwen <mian@cyverse.org>
2016-08-30 13:59:42 -0700

    Add :deploy-repositories for authy

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/cyverse-de/authy"
   :license {:name "BSD Standard License"
             :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [clj-http "2.0.0"]
