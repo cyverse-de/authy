44947ed7b6d101b548c6d91c332bd13c3a43dce1
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-23 13:09:42 -0700

    CORE-2006: updated dependencies and .gitignore

--- project.clj
+++ project.clj
@@ -2,11 +2,13 @@
   :description "An OAuth 2.0 client clibrary written in Clojure."
   :url "https://github.com/cyverse-de/authy"
   :license {:name "BSD Standard License"
-            :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+            :url "https://cyverse.org/license"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :plugins [[test2junit "1.2.2"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [clj-http "2.0.0"]
-                 [slingshot "0.10.3"]])
+  :plugins [[jonase/eastwood "1.4.3"]
+            [lein-ancient "0.7.0"]
+            [test2junit "1.4.4"]]
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [org.clojure/tools.logging "1.3.0"]
+                 [clj-http "3.13.0"]
+                 [slingshot "0.12.2"]])
