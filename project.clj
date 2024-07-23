62b10a3f6ed052025ebc46a2cc704f657b12d7a8
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-23 13:20:16 -0700

    3.0.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/authy "3.0.1-SNAPSHOT"
+(defproject org.cyverse/authy "3.0.1"
   :description "An OAuth 2.0 client clibrary written in Clojure."
   :url "https://github.com/cyverse-de/authy"
   :license {:name "BSD Standard License"
